d7a29ecc6f9b26adcbb7bc56fff8a750f09fb30a
Andrew Nava-Juarez <anava014@ucr.edu>
2015-05-30 10:57:43 -0700

    Sensitivity Changes
    Misfired alerts were being sent so I added logic for detecting multiple
    movements until trigger instead of just one.

--- motionToText_Arduino/motionToText_Arduino.ino
+++ motionToText_Arduino/motionToText_Arduino.ino
@@ -1,38 +1,47 @@
 #include <Bridge.h>
 #include <HttpClient.h>
 
+#define SENSITIVITY 4
+
 int pirPin = 6;
 int ledPin = 9;
+int movementCounter = 0;
+int movementTimer = 0;
 boolean movement = false;
 
 void setup() {
-  // put your setup code here, to run once:
   Bridge.begin();
   pinMode(pirPin, INPUT);
   pinMode(ledPin, OUTPUT);
 }
 
 void motionDetect () {
-  
   movement = digitalRead(pirPin);
   if (movement == true) {
-      digitalWrite(ledPin, HIGH);
-      
-      /*client connects to Raspberry Pi which has its own server and 
-      uses php to receive a trigger that then runs an exec command */
-      HttpClient client;
-      client.get("192.168.1.109/security.php?trigger=1");
-      
-      //Delay for 30 seconds to avoid spamming of text messages
-      delay(30000);
-      digitalWrite(ledPin, LOW);
+      ++movementCounter;
+      movementTimer = millis();
+      if(movementCounter >= SENSITIVITY){
+        digitalWrite(ledPin, HIGH);
+        
+        /*client connects to Raspberry Pi which has its own server and 
+        uses php to receive a trigger that then runs an exec command */
+        HttpClient client;
+        client.get("192.168.1.109/security.php?trigger=1");
+        
+        //Delay for 30 seconds to avoid spamming of text messages
+        delay(30000);
+        digitalWrite(ledPin, LOW);
+      }
   }
 }
   
+void motionDetectorReset(){
+  if(movementCounter > 0 && millis() > movementTimer + 10000)
+    movementCounter = 0;
+}
   
 void loop() {
   motionDetect ();
-
+  motionDetectorReset();
   delay(3000);
-
 }
